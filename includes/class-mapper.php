b4efc42bc9ca348f000ee2cded7d6f0bf360b388
Mike Pretzlaw <pretzlaw@gmail.com>
2016-08-09 09:23:26 +0200

    Mapper helper to easily map data

---
+++ includes/class-mapper.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace WP_FSI;
+
+class Mapping extends \ArrayObject {
+	function __invoke( $data ) {
+		return $this->parse( $data );
+	}
+
+	public function apply( $data ) {
+		$map = array();
+
+		foreach ( $this->getArrayCopy() as $key => $value ) {
+			if ( is_scalar( $value ) ) {
+				if ( is_array( $data ) && isset( $data[ $value ] ) ) {
+					$map[ $key ] = $data[ $value ];
+				} elseif ( is_object( $data ) && isset( $data->$value ) ) {
+					$map[ $key ] = $data->$value;
+				}
+
+				continue;
+			}
+
+			if ( is_scalar( $value ) ) {
+				$map[ $key ] = $value;
+				continue;
+			}
+
+			if ( is_callable( $value ) ) {
+				$map[ $key ] = $value( $data );
+			}
+		}
+
+		return $map;
+	}
+
+}
