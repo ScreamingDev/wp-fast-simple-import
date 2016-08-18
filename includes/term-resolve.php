ca6c0b822666ccbf7e185a4cf343b780390164d7
Mike Pretzlaw <pretzlaw@gmail.com>
2016-08-18 11:35:14 +0200

    Resolve terms

--- includes/term-resolve.php
+++ includes/term-resolve.php
@@ -9,3 +9,46 @@ function fsi_term_resolve( $name, $taxonomy = 'category' ) {
 
 	return false;
 }
+
+/**
+ * @param string $taxonomy
+ * @param string $field
+ * @param string $value
+ *
+ * @return array
+ */
+function fsi_get_all_terms_by( $taxonomy, $field, $value ) {
+	$query = array( 'taxonomy' => $taxonomy, 'hide_empty' => false );
+
+	if ( property_exists( WP_Term::class, $field ) ) {
+		$query[ $field ] = $value;
+	} else {
+		$query['meta_query'] = array(
+			array(
+				'key'   => $field,
+				'value' => $value,
+			),
+		);
+	}
+
+	$terms = get_terms( $query );
+
+	if ( is_wp_error( $terms ) || is_int( $terms ) ) {
+		return array();
+	}
+
+	return $terms;
+}
+
+/**
+ * @param $taxonomy
+ * @param $field
+ * @param $value
+ *
+ * @return null|WP_Term
+ */
+function fsi_get_term_by( $taxonomy, $field, $value ) {
+	$result = (array) fsi_get_all_terms_by( $taxonomy, $field, $value );
+
+	return current( $result );
+}
