4dfdebb1ce996404d28e04e94f1ebcfd32fb75b1
Mike Pretzlaw <pretzlaw@gmail.com>
2016-12-09 09:12:10 +0100

    Clean up code / Documentation
    Enhanced the NPath a bit.
    Added documentation.

--- includes/class-mapper.php
+++ includes/class-mapper.php
@@ -8,30 +8,43 @@ class Mapping extends \ArrayObject {
 	}
 
 	public function apply( $data ) {
-		$map = array();
+		$output = array();
 
 		foreach ( $this->getArrayCopy() as $key => $value ) {
 			if ( is_scalar( $value ) ) {
+				// This is some simple data so at first we store it directly in the output.
+				$output[ $key ] = $value;
+				
+				// Check if it was meant as an index hash.
 				if ( is_array( $data ) && isset( $data[ $value ] ) ) {
-					$map[ $key ] = $data[ $value ];
+					// Data has been found in an array so we fetch it from there.
+					$output[ $key ] = $data[ $value ];
 				} elseif ( is_object( $data ) && isset( $data->$value ) ) {
-					$map[ $key ] = $data->$value;
+					// Data has been found in an object so we fetch it from there.
+					$output[ $key ] = $data->$value;
 				}
 
 				continue;
 			}
 
-			if ( is_scalar( $value ) ) {
-				$map[ $key ] = $value;
+			if ( is_callable( $value ) ) {
+				// Found a callable that wants to handle the data.
+				$output[ $key ] = $value( $this, $data, $output );
+				
 				continue;
 			}
-
-			if ( is_callable( $value ) ) {
-				$map[ $key ] = $value( $this, $data );
-			}
+			
+			// Unknown what came in so we throw an exception.
+			throw new \InvalidArgumentException(
+				sprintf(
+					'Unhandled type of mapping for "%s": %s. Please use a string or a callable.',
+					$key,
+					gettype( $value )
+				)
+			);
 		}
 
-		return $map;
+		return $output;
 	}
 
 }
